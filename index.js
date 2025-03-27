6f4658a188d951a53e5c3ab6bb6da172f731bb82
Jagrat parnami <parnami.jagrat.com>
2025-03-27 21:11:49 +0530

    commit

--- index.js
+++ index.js
@@ -1,25 +1,68 @@
-
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 // import express from "express"
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const githubdata = {
+  login: "hiteshchoudhary",
+  id: 11613311,
+  node_id: "MDQ6VXNlcjExNjEzMzEx",
+  avatar_url: "https://avatars.githubusercontent.com/u/11613311?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/hiteshchoudhary",
+  html_url: "https://github.com/hiteshchoudhary",
+  followers_url: "https://api.github.com/users/hiteshchoudhary/followers",
+  following_url:
+    "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+  gists_url: "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+  subscriptions_url:
+    "https://api.github.com/users/hiteshchoudhary/subscriptions",
+  organizations_url: "https://api.github.com/users/hiteshchoudhary/orgs",
+  repos_url: "https://api.github.com/users/hiteshchoudhary/repos",
+  events_url: "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/hiteshchoudhary/received_events",
+  type: "User",
+  user_view_type: "public",
+  site_admin: false,
+  name: "Hitesh Choudhary",
+  company: null,
+  blog: "https://www.youtube.com/c/HiteshChoudharydotcom",
+  location: "India",
+  email: null,
+  hireable: null,
+  bio: "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+  twitter_username: "hiteshdotcom",
+  public_repos: 103,
+  public_gists: 5,
+  followers: 40886,
+  following: 0,
+  created_at: "2015-03-23T13:03:25Z",
+  updated_at: "2025-03-21T11:48:40Z",
+};
 
-app.get('/twitter',(req,res)=>{
-    res.send('jpinsta')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/login',(req,res)=>{
-    res.send("<h1> please login at chai aur code</h1>")
-})
+app.get("/twitter", (req, res) => {
+  res.send("jpinsta");
+});
 
-app.get('/youtube',(req,res)=>{
-    res.send('<h2> chai aur code <h2>')
-})
+app.get("/login", (req, res) => {
+  res.send("<h1> please login at chai aur code</h1>");
+});
+
+app.get("/github", (req, res) => {
+  res.json(githubdata);
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2> chai aur code <h2>");
+});
 app.listen(process.env.port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
